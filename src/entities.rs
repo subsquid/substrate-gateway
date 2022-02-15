cf0216a86a8b4e032cd75c8d2ed85651f87e4344
tmcgroul <safonov.a.r@mail.ru>
2022-02-15 11:49:45 +0300

    add extrinsic, call, event to api

--- src/entities.rs
+++ src/entities.rs
@@ -9,3 +9,40 @@ pub struct Block {
     parent_hash: String,
     timestamp: DateTime<Utc>,
 }
+
+
+#[derive(sqlx::FromRow, async_graphql::SimpleObject)]
+pub struct Extrinsic {
+    id: String,
+    block_id: String,
+    index_in_block: i32,
+    name: String,
+    signature: Option<serde_json::Value>,
+    success: bool,
+    hash: Vec<u8>,
+}
+
+
+#[derive(sqlx::FromRow, async_graphql::SimpleObject)]
+pub struct Call {
+    id: String,
+    index: i32,
+    extrinsic_id: String,
+    parent_id: Option<String>,
+    success: bool,
+    name: String,
+    args: Option<serde_json::Value>,
+}
+
+
+#[derive(sqlx::FromRow, async_graphql::SimpleObject)]
+pub struct Event {
+    id: String,
+    block_id: String,
+    index_in_block: i32,
+    phase: String,
+    extrinsic_id: Option<String>,
+    call_id: Option<String>,
+    name: String,
+    args: Option<serde_json::Value>
+}
--- src/graphql.rs
+++ src/graphql.rs
@@ -1,4 +1,4 @@
-use super::entities::Block;
+use super::entities::{Block, Extrinsic, Call, Event};
 use async_graphql::{FieldResult, Object, Context};
 
 
@@ -8,12 +8,48 @@ pub struct QueryRoot;
 #[Object]
 impl QueryRoot{
     async fn block(&self, ctx: &Context<'_>, limit: Option<i32>) -> FieldResult<Vec<Block>> {
-        let pool = ctx.data::<sqlx::Pool<sqlx::Postgres>>().unwrap();
+        let pool = ctx.data::<sqlx::Pool<sqlx::Postgres>>()?;
         let mut query = String::from("SELECT * FROM block");
         if let Some(limit) = limit {
             query.push_str(&format!(" LIMIT {}", limit));
         }
         let result = sqlx::query_as::<_, Block>(&query)
+            .fetch_all(pool)
+            .await?;
+        Ok(result)
+    }
+
+    async fn extrinsic(&self, ctx: &Context<'_>, limit: Option<i32>) -> FieldResult<Vec<Extrinsic>> {
+        let pool = ctx.data::<sqlx::Pool<sqlx::Postgres>>()?;
+        let mut query = String::from("SELECT * FROM extrinsic");
+        if let Some(limit) = limit {
+            query.push_str(&format!(" LIMIT {}", limit));
+        }
+        let result = sqlx::query_as::<_, Extrinsic>(&query)
+            .fetch_all(pool)
+            .await?;
+        Ok(result)
+    }
+
+    async fn call(&self, ctx: &Context<'_>, limit: Option<i32>) -> FieldResult<Vec<Call>> {
+        let pool = ctx.data::<sqlx::Pool<sqlx::Postgres>>()?;
+        let mut query = String::from("SELECT * FROM call");
+        if let Some(limit) = limit {
+            query.push_str(&format!(" LIMIT {}", limit));
+        }
+        let result = sqlx::query_as::<_, Call>(&query)
+            .fetch_all(pool)
+            .await?;
+        Ok(result)
+    }
+
+    async fn event(&self, ctx: &Context<'_>, limit: Option<i32>) -> FieldResult<Vec<Event>> {
+        let pool = ctx.data::<sqlx::Pool<sqlx::Postgres>>()?;
+        let mut query = String::from("SELECT * FROM event");
+        if let Some(limit) = limit {
+            query.push_str(&format!(" LIMIT {}", limit));
+        }
+        let result = sqlx::query_as::<_, Event>(&query)
             .fetch_all(pool)
             .await?;
         Ok(result)

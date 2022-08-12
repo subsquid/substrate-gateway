a9c74d48cc57aa5105c982325ddeb28dcf30c21e
tmcgroul <safonov.a.r@mail.ru>
2022-08-12 16:37:34 +0300

    fix EvmExecuted matching function

--- src/archive/postgres/selection.rs
+++ src/archive/postgres/selection.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 use crate::entities::{Call, Event, ContractsEvent};
 use crate::archive::{
     CallSelection, EventSelection, ContractsEventSelection, EthTransactSelection,
@@ -78,7 +79,9 @@ impl EvmExecutedSelection {
                     for log in logs {
                         if let Some(value) = log.get("address") {
                             if let Some(address) = value.as_str() {
-                                return address == self.contract
+                                if address == self.contract && self.filter_match(log) {
+                                    return true;
+                                }
                             }
                         }
                     }
@@ -86,5 +89,38 @@ impl EvmExecutedSelection {
             }
         }
         false
+    }
+
+    fn filter_match(&self, log: &Value) -> bool {
+        let filter: Vec<_> = self.filter.iter().enumerate().collect();
+        for (index, topics) in filter {
+            if !self.topics_match(topics, log, index) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn topics_match(&self, topics: &Vec<String>, log: &Value, index: usize) -> bool {
+        if topics.is_empty() {
+            return true
+        }
+
+        if let Some(log_topic) = self.get_log_topic(log, index) {
+            topics.iter().any(|topic| topic == &log_topic)
+        } else {
+            false
+        }
+    }
+
+    fn get_log_topic(&self, log: &Value, index: usize) -> Option<String> {
+        if let Some(value) = log.get("topics") {
+            if let Some(value) = value.get(index) {
+                if let Some(topic) = value.as_str() {
+                    return Some(topic.to_string())
+                }
+            }
+        }
+        None
     }
 }
